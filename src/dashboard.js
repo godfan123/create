56a9c31b64140eadccf4a75eee11c8ace13b9999
godfan123 <1907915060@qq.com>
2018-01-02 15:58:10 +0800

    Update dashboard.js

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link,Route} from 'react-router-dom';
+import {Link,Route,Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {logout} from '../redux/auth.redux';
 import App from './app';
 function Erying(){
     return <h2>无状态组件2</h2>
@@ -8,32 +10,42 @@ function Qi() {
     return <h2>无状态组件3</h2>
 }
 
+@connect(
+    state=>state.auth,
+    {logout}
+    )
+
+
 class Dashboard extends React.Component{
     constructor(props){
         super(props)
     }
 
     render(){
-        return(
-           <div>
-               <ul>
-                   <li>
-                       <Link to="/dashboard" >1</Link>
-                   </li>
-                   <li>
-                       <Link to="/dashboard/erying" >2</Link>
-                   </li>
-                   <li>
-                       <Link to="/dashboard/qibinglian" >3</Link>
-                   </li>
-                   <Route path="/dashboard/" exact component={App}></Route>
-                   <Route path="/dashboard/erying" component={Erying}></Route>
-                   <Route path="/dashboard/qibinglian" component={Qi}></Route>
-               </ul>
-           </div>
+        const redirectorToLogin=<Redirect to="./login"></Redirect>
+        const app=(<div>
+                <div>page </div>
+                {this.props.isAuth ? <button onClick={this.props.logout}>注销</button> : null}
+            <ul>
+                <li>
+                    <Link to="/dashboard" >1</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard/erying" >2</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard/qibinglian" >3</Link>
+                </li>
+                <Route path="/dashboard/" exact component={App}></Route>
+                <Route path="/dashboard/erying" component={Erying}></Route>
+                <Route path="/dashboard/qibinglian" component={Qi}></Route>
+            </ul>
+        </div>);
 
-        )
 
+
+      /*  return this.props.isAuth ? app : redirectorToLogin*/
+        return app
     }
 
 }
