7d429fa8aa28bbe3aa2beba9b5b286efa85629e3
godfan123 <1907915060@qq.com>
2018-01-05 19:29:20 +0800

    Update index.redux.js

--- redux/index.redux.js
+++ redux/index.redux.js
@@ -1,29 +1,39 @@
-const AGG='增加';
-const REMOVE='减少';
-export  function counter(state=10,action){
+const ADD="增加";
+const REMOVE="减少";
 
-    switch(action.type){
-        case AGG:
-            return state+1;
-        case REMOVE:
-            return state-1;
-        default:
-            return state
+export function counter(state=10,action){
+      switch(action.type){
+          case ADD:
+              return state+1;
+          case REMOVE:
+              return state-1;
+
+          default:
+              return state
+      }
+}
+
+
+export function add(){
+     return {type:ADD}
     }
 
-}
 
-export function addGun() {
-     return {type:AGG}
-}
-export function removeGun(){
+export function remove(){
     return {type:REMOVE}
 }
-export function addGunAsync(){
+export function removeTime(){
+
     return dispatch=>{
+
         setTimeout(()=>{
-            dispatch(addGun())
+            dispatch({type:ADD})
+
         },2000)
+
     }
+
+
 }
+
 
