9deb62d5088ca62eaa97d9d7df17b524c5bf6b0c
godfan123 <1907915060@qq.com>
2018-01-05 19:25:27 +0800

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {login} from '../redux/auth.redux';
+import {Button} from 'antd-mobile';
 
 @connect(
     state=>state.auth,
@@ -16,15 +17,12 @@ class Auth extends React.Component{
                 <div>
                     {this.props.isAuth ? <Redirect to="/dashboard"/> : null}
                    <h2>你没有权限需要登入才能看</h2>
-                    <button onClick={this.props.login}>登入</button>
+                    <Button onClick={this.props.login}>登入</Button>
+
                </div>
           )
     }
 }
-
-
-
-
 
 
 export default Auth;
